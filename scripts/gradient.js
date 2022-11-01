c41dc21c81f22309d115a33b288551217a989a20
Tom Carleton <tomcarletonmusic@gmail.com>
2022-11-01 21:37:07 +0000

    Gradient changes
    Changes to gradient background, refreshing starts from a random seed.

--- scripts/gradient.js
+++ scripts/gradient.js
@@ -1,12 +1,13 @@
 const c = document.getElementById('canv');
 const $ = c.getContext('2d');
+const startVal = 50 * Math.random();
 
 const col = function(x, y, r, g, b) {
   $.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
   $.fillRect(x, y, 1,1);
 }
 const R = function(x, y, t) {
-  return( Math.floor(192 + 64*Math.cos( (x*x-y*y)/300 + t )) );
+  return( Math.floor(100 + 32*Math.cos( (x*x-y*y)/200 + t/2 )) );
 }
 
 const G = function(x, y, t) {
@@ -17,17 +18,17 @@ const B = function(x, y, t) {
   return( Math.floor(192 + 64*Math.sin( 5*Math.sin(t/9) + ((x-100)*(x-100)+(y-100)*(y-100))/1100) ));
 }
 
-let t = 0;
+let t = startVal;
 let x;
 let y;
 
 const run = function() {
   for(x=0;x<=35;x++) {
     for(y=0;y<=35;y++) {
-      col(x, y, R(x,y,t) * 0.4, G(x,y,t) * 0.8, B(x,y,t));
+      col(x, y, R(x,y,t), G(x,y,t), B(x,y,t));
     }
   }
-  t = t + 0.007;
+  t = t + 0.01;
   window.requestAnimationFrame(run);
 }
 
